18613ac7b4536af35307c69c49285814ee1d99b2
Maximilian Bauregger <maximilian.bauregger@studium.uni-hamburg.de>
2020-11-05 13:09:58 +0100

    Notify user if /poll does not find any updates

--- telegram_bot.py
+++ telegram_bot.py
@@ -40,14 +40,7 @@ Currently the following states are considered: {battlegrounds}.
 """)
 
 
-def check(context):
-    """Crawl api and notfy "subscribers" if votes changed"""
-
-    dp = context.dispatcher
-    user_data = dp.user_data
-
-    chat_id = context.job.context
-
+def _check(user_data, battlegrounds):
     new = parse_data(get_data())
     old = user_data.get('old')
 
@@ -57,10 +50,8 @@ def check(context):
 
     for state in filter_states(old, new, battlegrounds):
 
-        txt = textify_change(
+        yield textify_change(
             state=state, old=old[state], new=new[state], candidates=candidates)
-
-        context.bot.send_message(chat_id, text=txt)
 
         old[state] = new[state]
     else:
@@ -69,6 +60,19 @@ def check(context):
     user_data['old'] = old
 
     return True
+
+
+
+def check(context):
+    """Crawl api and notfy "subscribers" if votes changed"""
+
+    dp = context.dispatcher
+    user_data = dp.user_data
+
+    chat_id = context.job.context
+
+    for txt in _check(user_data, battlegrounds):
+        context.bot.send_message(chat_id, text=txt)
 
 
 def remove_job_if_exists(name, context):
@@ -117,7 +121,11 @@ def cancel(update: Update, context: CallbackContext) -> None:
 def poll(update: Update, context: CallbackContext) -> None:
     """Allow the user to cancel the updates"""
     chat_id = update.message.chat_id
-    context.job_queue.run_once(check, 0, context=chat_id, name=f"{chat_id}_poll")
+
+    for txt in _check(context.user_data, battlegrounds):
+        update.message.reply_text(txt)
+    else:
+        update.message.reply_text("No changes found")
 
 
 def info(update: Update, context: CallbackContext) -> None:
